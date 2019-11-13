27c1fe6965d1af704dd622cc3f91917ba3d772e0
lafith <lafith@lafith.mshome.net>
2019-11-13 22:49:13 +0530

    data_publisher added

--- sim_gazebo/src/data_publisher.cpp
+++ sim_gazebo/src/data_publisher.cpp
@@ -1,116 +1,55 @@
-#ifndef _DATA_PUBLISHER_HH_
-#define _DATA_PUBLISHER_HH_
-
-#include <gazebo/gazebo.hh>
-#include <gazebo/physics/physics.hh>
-#include <thread>
 #include "ros/ros.h"
-#include "ros/callback_queue.h"
-#include "ros/subscribe_options.h"
-#include "std_msgs/Float32.h"
-#include <gazebo/transport/transport.hh>
-#include <gazebo/msgs/msgs.hh>
-#include <functional>
-#include <gazebo/common/common.hh>
-#include <ignition/math/Vector3.hh>
-#include<string>
-#include<iostream>
+#include "geometry_msgs/Vector3Stamped.h"
+#include "geometry_msgs/PointStamped.h"
+#include "geometry_msgs/Pose.h"
+#include "gazebo_msgs/ModelState.h"
+#include "gazebo_msgs/ModelStates.h"
 #include<sim_gazebo/Combined.h>
-#include <thread>
-#include <chrono>
+#include <cmath>
 #include <string>
-#include <vector>
-#include <boost/scoped_ptr.hpp>
-#include <boost/algorithm/string.hpp>
+using std::vector;
+using std::string;
 
-namespace gazebo
-{
-  /// \brief A plugin to control a Velodyne sensor.
-  class DataPublisher : public ModelPlugin
+ros::Publisher rosPub;
+
+void modelStatesCallback(const gazebo_msgs::ModelStates& msg){
+  sim_gazebo::Combined msg_pub;
+  int auv_index = -1;
+  for(int i = 0; i < msg.name.size(); i++)
   {
-    //private: std::unique_ptr<ros::NodeHandle> rosNode;
+      if (msg.name[i] == "auv") auv_index = i;
+  }
 
-    /// \brief A ROS subscriber
-    private: ros::Publisher rosPub;
-    private: sim_gazebo::Combined msg;
-    /// \brief Pointer to the model.
-    private: physics::ModelPtr auv;
-    private: physics::LinkPtr hull;
-    private: gazebo::event::ConnectionPtr updateConnection;
-    private: gazebo::physics::WorldPtr sauvc;
-    private: boost::shared_ptr<ros::NodeHandle> node;
-    private: double updateRate;
-    private: double updatePeriod;
-    private: gazebo::common::Time lastUpdate;
+  if (auv_index == -1)
+  {
+      ROS_ERROR_STREAM("Failed to locate auv model state.");
+      return;
+  }
+    msg_pub.angular[0]=msg.pose[auv_index].orientation.x;
+    msg_pub.angular[1]=msg.pose[auv_index].orientation.y;
+    msg_pub.angular[2]=msg.pose[auv_index].orientation.z;
+    msg_pub.linear[0]=msg.pose[auv_index].position.x;
+    msg_pub.linear[1]=msg.pose[auv_index].position.y;
+    msg_pub.linear[2]=msg.pose[auv_index].position.z;
+    msg_pub.depth=msg.pose[auv_index].position.z;
+    rosPub.publish(msg_pub);
 
-    public: void Load(physics::ModelPtr _auv, sdf::ElementPtr _sdf)
-    {
-      //std::this_thread::sleep_for(std::chrono::milliseconds(1000*100));
-      this->auv = _auv;
-      //output message:
-      GZ_ASSERT(this->auv != NULL, "Invalid model pointer");
-      std::cerr << "\nThe data_publisher plugin is attach to model[" <<_auv->GetName() << "]\n";
-      this->sauvc = this->auv->GetWorld();
-      // Initialize ros, if it has not already bee initialized.
-      if (!ros::isInitialized())
-      {
-        gzerr << "ROS was not initialized. Closing plugin..." << std::endl;
-        return;
-      }
-      this->node = boost::shared_ptr<ros::NodeHandle>(new ros::NodeHandle("gazebo_publish"));
-      this->hull=_auv->GetChildLink("auv::auv::hull");
-
-      if (_sdf->HasElement("updateRate"))
-        this->updateRate = _sdf->Get<double>("updateRate");
-      else
-        this->updateRate = 50;
-
-
-      GZ_ASSERT(this->updateRate > 0, "Update rate must be positive");
-      // Setting the update period
-      this->updatePeriod = 1.0 / this->updateRate;
-      this->rosPub =this->node->advertise<sim_gazebo::Combined>("/synchronizer/combined", 1);
-
-    #if GAZEBO_MAJOR_VERSION >= 8
-        this->lastUpdate = this->sauvc->SimTime();
-      #else
-        this->lastUpdate = this->sauvc->GetSimTime();
-      #endif
-
-      // Connect the update function to the Gazebo callback
-      this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
-        boost::bind(&DataPublisher::Onupdate, this, _1));
-        //std::cerr<<this->hull->WorldPose().Rot().X();
-      //this->rosNode.reset(new ros::NodeHandle("gazebo_publish"));
-      //this->rosPub=this->rosNode->advertise<auvsim_gazebo::Combined>("/synchronizer/combined",1);
-      //update_event_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&DataPulisher::OnUpdate, this));
-      //std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-      //this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&DataPulisher::onUpdate, this));
-    }
-
-    public: void Onupdate(const gazebo::common::UpdateInfo &_info){
-      #if GAZEBO_MAJOR_VERSION >= 8
-        gazebo::common::Time simTime = this->sauvc->SimTime();
-      #else
-        gazebo::common::Time simTime = this->sauvc->GetSimTime();
-      #endif
-
-      if (simTime - this->lastUpdate >= this->updatePeriod)
-      {
-        this->msg.angular[0]=this->hull->GetWorldPose().rot.x;
-        this->msg.angular[1]=this->hull->GetWorldPose().rot.y;
-        this->msg.angular[2]=this->hull->GetWorldPose().rot.z;
-        this->msg.linear[0]=this->hull->GetWorldPose().pos.x;
-        this->msg.linear[1]=this->hull->GetWorldPose().pos.y;
-        this->msg.linear[2]=this->hull->GetWorldPose().pos.z;
-        this->msg.depth=-(this->hull->GetWorldPose().pos.z);
-        this->rosPub.publish(msg);
-        this->lastUpdate = simTime;
-    }
-      //buoyancy:
-
-    }
-  };
-  GZ_REGISTER_MODEL_PLUGIN(DataPublisher)
 }
-#endif
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "data_publisher");
+    ros::NodeHandle nh;
+    ros::Subscriber auv_orient = nh.subscribe("gazebo/model_states", 1, modelStatesCallback);
+    rosPub=nh.advertise<sim_gazebo::Combined>("synchronizer/combined",1);
+    double rate=30;
+    ros::Rate r(rate);
+    
+    while(ros::ok())
+    {
+        ros::spinOnce();
+        r.sleep();
+    }
+
+    return 0;
+}
